14f569fe273db1e0f2cec82f58e4e2025e9aa337
Andrey Nelepov <nelepow@gmail.com>
2019-07-31 16:50:02 -0400

    deleting firebase script

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,14 +14,14 @@ class Home extends Component {
         </div>
         <>
           <h1>
-            Home<span class="badge badge-secondary">New</span>
+            Home<span className="badge badge-secondary">New</span>
           </h1>
-          <ul class="list-group">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
+          <ul className="list-group">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Morbi leo risus</li>
+            <li className="list-group-item">Porta ac consectetur ac</li>
+            <li className="list-group-item">Vestibulum at eros</li>
           </ul>
         </>
       </div>
