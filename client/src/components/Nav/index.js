cfcb1c11d3bb0a589ae4c41c79d8449f35ae9e15
Andrey Nelepov <nelepow@gmail.com>
2019-07-30 20:55:47 -0400

    installed mpn bootstrap reactprime

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+      <a className="navbar-brand" href="/">
+        Weight Tracker
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
