0cd4bdb1270e8e0d172b6c4871f6859de364c72b
Andrey Nelepov <nelepow@gmail.com>
2019-07-31 18:10:38 -0400

    added panel rprime react

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
-// import React from "react";
-// import app from "../base";
 import Nav from "../components/Nav";
 import React, { Component } from "react";
+// import { Panel } from "primereact/panel";
+import { Panel } from "../components/Panel";
 
 class Home extends Component {
+  state = {
+    panelCollapsed: true
+  };
   render() {
     return (
       <div>
@@ -12,39 +15,42 @@ class Home extends Component {
             <Nav />
           </div>
         </div>
-        <>
-          <h1>
-            Home<span className="badge badge-secondary">New</span>
-          </h1>
-          <ul className="list-group">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Morbi leo risus</li>
-            <li className="list-group-item">Porta ac consectetur ac</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
-        </>
+        <div className="container">
+          <Panel
+            header="Joke of a Day"
+            style={{ marginTop: "2em" }}
+            toggleable={true}
+            collapsed={this.state.panelCollapsed}
+            onToggle={e => this.setState({ panelCollapsed: e.value })}
+          >
+            The story begins as Don Vito Corleone, the head of a New York Mafia
+            family, oversees his daughter's wedding. His beloved son Michael has
+            just come home from the war, but does not intend to become part of
+            his father's business. Through Michael's life the nature of the
+            family business becomes clear. The business of the family is just
+            like the head of the family, kind and benevolent to those who give
+            respect, but given to ruthless violence whenever anything stands
+            against the good of the family.
+          </Panel>
+          <Panel header="Home" style={{ marginTop: "2em" }} toggleable={true}>
+            <h1>
+              Home<span className="badge badge-secondary">New</span>
+            </h1>
+          </Panel>
+          <Panel header="Table" style={{ marginTop: "2em" }} toggleable={true}>
+            The story begins as Don Vito Corleone, the head of a New York Mafia
+            family, oversees his daughter's wedding. His beloved son Michael has
+            just come home from the war, but does not intend to become part of
+            his father's business. Through Michael's life the nature of the
+            family business becomes clear. The business of the family is just
+            like the head of the family, kind and benevolent to those who give
+            respect, but given to ruthless violence whenever anything stands
+            against the good of the family.
+          </Panel>
+        </div>
       </div>
     );
   }
 }
-
-// const Home = () => {
-//   return (
-//     <>
-//       <h1>Home</h1>
-//       <button onClick={() => app.auth().signOut()}>Sign out</button>
-//     </>
-//   );
-//    render() {
-//     return (
-//       <div className="App">
-//         <div className="Nav">
-//           <Nav />
-//         </div>
-//       </div>
-//     );
-//   }
-// };
 
 export default Home;
