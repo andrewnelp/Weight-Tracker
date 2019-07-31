6332e9e0dbb8cceb3110165fac402c2645d2655e
Andrey Nelepov <nelepow@gmail.com>
2019-07-31 14:59:07 -0400

    adding auth firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { AuthProvider } from "./Auth";
+import PrivateRoute from "./PrivateRoute";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="Nav">
-          <Nav />
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
         </div>
-      </div>
-    );
-  }
-}
+      </Router>
+    </AuthProvider>
+  );
+};
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="Nav">
+//           <Nav />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
