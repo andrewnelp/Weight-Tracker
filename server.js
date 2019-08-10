4cc3b5f925fdad572dace7dc1afc7e54b924abc7
Andrey Nelepov <nelepow@gmail.com>
2019-08-10 10:57:29 -0400

    prepared for deployment

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require("path");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 
 //Connect DB
